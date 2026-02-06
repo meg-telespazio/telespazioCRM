33d8d01743f033bc99b5e61fca81c88226ae8248
Mariano Gonzalez <mariano.telespazio@gmail.com>
2026-02-06 14:37:16 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse gap-4 p-4 sm:top-4 sm:mx-auto md:max-w-lg",
+      "fixed top-4 left-1/2 z-[100] flex w-full max-w-lg -translate-x-1/2 flex-col-reverse gap-4",
       className
     )}
     {...props}
