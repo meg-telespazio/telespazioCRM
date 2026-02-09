cb6020d5e10a0a36981011967c1866f85d13fb5d
Mariano Gonzalez <mariano.telespazio@gmail.com>
2026-02-09 02:29:05 +0000

    Try fixing this error: `Console TypeError: Cannot read properties of und

--- src/app/opportunities/[id]/print/page.tsx
+++ src/app/opportunities/[id]/print/page.tsx
@@ -14,7 +14,7 @@ import { collection, query, where, doc } from 'firebase/firestore';
 import { format, addDays } from 'date-fns';
 import { es, enUS } from 'date-fns/locale';
 import Image from 'next/image';
-import { numeroALetras } from 'numero-a-letras';
+import numeroALetras from 'numero-a-letras';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { Skeleton } from '@/components/ui/skeleton';
