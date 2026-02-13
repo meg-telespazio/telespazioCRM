7bebe11cc5e712d9d824874092b249d274b157ee
Mariano Gonzalez <mariano.telespazio@gmail.com>
2026-02-13 03:11:36 +0000

    ok, el tema es que deberias limitar el ancho del los cards que contienen

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -81,7 +81,7 @@ export default function DashboardPage() {
   return (
     <div className="flex flex-1 flex-col">
       <AppHeader title={t('Dashboard.title')} />
-      <div className="flex-1 space-y-4 p-4 sm:p-6">
+      <div className="flex-1 space-y-4 p-4 sm:p-6 overflow-hidden">
         {pageIsLoading ? (
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
             <Skeleton className="h-28" />
