645d5ffa63a822d31ab7e80d5485624c114518a1
Mariano Gonzalez <mariano.telespazio@gmail.com>
2026-02-18 23:30:58 +0000

    Try fixing this error: `Runtime Error: Body exceeded 1 MB limit. To conf

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   /* config options here */
   serverActions: {
-    bodySizeLimit: '15mb',
+    bodySizeLimit: '30mb',
   },
   typescript: {
     ignoreBuildErrors: true,
