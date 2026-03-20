84bb7b6b3686f70f58759482b10a2249f795bb94
Mariano Gonzalez <mariano.telespazio@gmail.com>
2026-03-20 19:41:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -38,6 +38,7 @@ import {
   TableRow 
 } from '@/components/ui/table';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Badge } from '@/components/ui/badge';
 import { 
   Dialog, 
   DialogContent, 
