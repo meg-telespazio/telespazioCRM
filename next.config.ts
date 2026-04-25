347e2ff7504a9d4fd7cadf0f4dc1de832982766a
meg-telespazio <mariano.telespazio@gmail.com>
2026-04-24 23:28:17 -0300

    Security: Add HSTS header (SEC-011)

--- next.config.ts
+++ next.config.ts
@@ -145,6 +145,10 @@ const nextConfig: NextConfig = {
             key: 'X-Frame-Options',
             value: 'SAMEORIGIN',
           },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
         ],
       },
     ];
